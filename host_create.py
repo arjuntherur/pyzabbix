55214ab793df87212b16b6d73ad749536495a636
ARJUN T PREM <33035998+arjuntherur@users.noreply.github.com>
2023-03-22 23:10:33 +0530

    Update host_create.py

--- host_create.py
+++ host_create.py
@@ -3,23 +3,23 @@ import requests
 import os
 import json
 
-#define username and password
-
+#Define username and password
 username = os.environ['ZABBIX_USERNAME']
 password = os.environ['ZABBIX_PASSWORD']
+
+#Connect to Zabbix 
 zapi = ZabbixAPI("https://zabbix.tsworkz.com/zabbix")
 zapi.login(username,password)
 print("Connected to Zabbix API Version %s" % zapi.api_version())
 
 
 # Define host parameters
-
 host_name = os.environ['host_name']
 host_group_id = 46 # ID of the host group the host will belong to
 template_id = 11071 # IDs of the templates the host will use
-tag_hostname = os.environ['tag_hostname']
+tag_host = os.environ['tag_hostname']
 tag_client_name = os.environ['tag_client_name']
-marco_url = "https://blog-test.dev-tsworks.io"
+marco_url = os.environ['macro_url']
 
 # Create host
 new_host = zapi.host.create(
@@ -36,8 +36,8 @@ new_host = zapi.host.create(
     ],
     tags= [
             {
-                "tag": "Host name",
-                "value": tag_hostname
+                "tag": "Host",
+                "value": tag_host
             },
             {
                 "tag": "Client Name",
