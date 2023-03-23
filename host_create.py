89444d922a967d16086bfab7f23c3e7e0b215b64
ARJUN T PREM <33035998+arjuntherur@users.noreply.github.com>
2023-03-23 13:25:10 +0530

    Update host_create.py

--- host_create.py
+++ host_create.py
@@ -18,8 +18,9 @@ host_name = os.environ['host_name']
 host_group_id = 46 # ID of the host group the host will belong to
 template_id = 11071 # IDs of the templates the host will use
 marco_url = os.environ['url']
-tag_host = os.environ['tag_hostname']
+tag_business = os.environ['tag_businessunit']
 tag_client_name = os.environ['tag_client_name']
+tag_type = os.environ['tag_type']
 
 
 # Create host
@@ -38,11 +39,15 @@ new_host = zapi.host.create(
     tags= [
             {
                 "tag": "Host",
-                "value": tag_host
+                "value": tag_business
             },
             {
                 "tag": "Client Name",
                 "value": tag_client_name
+            },
+            {
+                "tag": "Type",
+                "value": tag_type
             }
         ],
     macros=[
